5c09df3b77e86676e9a0cd9e2754417c1649081b
Damir Porobic <damir_porobic@live.com>
2018-02-21 21:12:33 +0100

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -36,12 +36,18 @@ int main(int argc, char** argv)
     app.setApplicationVersion(QStringLiteral(KSNIP_VERSION));
 
     QTranslator translator;
-    auto translationSuccessfullyLoaded = translator.load(QLocale(), QStringLiteral("ksnip"), QStringLiteral("_"), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    auto translationSuccessfullyLoaded = translator.load(QLocale(), QStringLiteral("ksnip"), QStringLiteral("_"), QStringLiteral("/usr/share/ksnip/translations/"));
+    if(!translationSuccessfullyLoaded) {
+        // If we can't load the translations from default location, check current directory
+
+        qCritical(QDir::currentPath().toLatin1());
+        translationSuccessfullyLoaded = translator.load(QLocale(), QStringLiteral("ksnip"), QStringLiteral("_"));
+    }
+
     if (translationSuccessfullyLoaded) {
         app.installTranslator(&translator);
     }
-    qCritical("Path for translation files");
-    qCritical(QLibraryInfo::location(QLibraryInfo::TranslationsPath).toLatin1());
+
 
     ImageGrabberFactory imageGrabberFactory;
     auto imageGrabber = imageGrabberFactory.createImageGrabber();

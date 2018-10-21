17535fb5ece22d409f488d0a6e283018605180a5
Damir Porobic <damir_porobic@live.com>
2018-10-21 15:12:08 +0200

    Update before attempt to install deps

--- ci/scripts/createDeb.sh
+++ ci/scripts/createDeb.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
+apt-get update
 apt-get install git cmake qt5-default libqt5x11extras5-dev qttools5-dev-tools extra-cmake-modules
-
-echo "building deb in "
-pwd
-echo "Content:"
-ls
 
 cd kColorPicker
 mkdir build && cd build
 cmake ..
-make && sudo make install
+make && make install
 sudo ldconfig
 cd ../..
 cd kImageAnnotator
 mkdir build && cd build
 cmake ..
-make && sudo make install
+make && make install
 sudo ldconfig
 cd ../..
 

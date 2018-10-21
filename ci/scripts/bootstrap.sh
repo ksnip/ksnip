1037649d8f7fc28de89bb9d4011ff99b2ef82f9d
Damir Porobic <damir_porobic@live.com>
2018-10-21 14:59:27 +0200

    Fix docker image name

--- ci/scripts/bootstrap.sh
+++ ci/scripts/bootstrap.sh
@@ -4,5 +4,5 @@ if [[ "${BUILD_TYPE}" == "AppImage" ]]; then
     sudo add-apt-repository ppa:beineri/opt-qt562-trusty -y
     sudo apt-get update -qq
 elif [[ "${BUILD_TYPE}" == "deb" ]]; then
-    docker pull ubuntu:17:10
+    docker pull ubuntu:17.10
 fi

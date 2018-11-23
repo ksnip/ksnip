457a8c6db84b342e9da5bc4ca1157ed069c6f0dd
Damir Porobic <damir_porobic@live.com>
2018-11-23 20:54:22 +0100

    Use nmake for building ksnip on windows

--- ci/scripts/install_dependencies.sh
+++ ci/scripts/install_dependencies.sh
@@ -2,11 +2,11 @@
 
 cd kColorPicker
 mkdir build && cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLE=OFF
 make && make install
 cd ../..
 cd kImageAnnotator
 mkdir build && cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLE=OFF
 make && make install
 cd ../..

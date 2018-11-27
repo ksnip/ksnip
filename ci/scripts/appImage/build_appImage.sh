03532b38375742f4890e1f2209be548f58e8fc21
Damir Porobic <damir_porobic@live.com>
2018-11-27 19:44:58 +0100

    Move appImage building to scripts

---
+++ ci/scripts/appImage/build_appImage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "--> Build "
+cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DVERSION_SUFIX=$VERSION_SUFFIX -DBUILD_NUMBER=$BUILD_NUMBER
+make -j$(nproc)
+make DESTDIR=appdir -j$(nproc) install ; find appdir/
+mkdir appdir/usr/bin ; mv appdir/bin/ksnip ./appdir/usr/bin/ ; rm -r ./appdir/bin
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+echo "--> Package appImage"
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines,imageformats

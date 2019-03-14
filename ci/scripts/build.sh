cf3cfb6f465598da239a39f005caebbe7ab81ba9
Damir Porobic <damir_porobic@live.com>
2019-03-14 10:58:08 +0100

    Remove cpu arch strings from release packages

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,13 +2,15 @@
 
 if [[ "${BUILD_TYPE}" == "AppImage" ]]; then
     source ci/scripts/appImage/build_appImage.sh
+    mv ksnip*.AppImage ksnip-$VERSION.AppImage
+    mv ksnip*.AppImage.zsync ksnip-$VERSION.AppImage.zsync
 elif [[ "${BUILD_TYPE}" == "deb" ]]; then
     docker exec build-container bash -c "source ci/scripts/deb/build_deb.sh"
-    mv ksnip_*.deb ksnip-$VERSION-amd64.deb
+    mv ksnip_*.deb ksnip-$VERSION.deb
 elif [[ "${BUILD_TYPE}" == "rpm" ]]; then
     docker exec build-container bash -c "source ci/scripts/rpm/build_rpm.sh"
     sudo chown -R 2000:2000 ksnip-$VERSION_NUMBER
-    mv ksnip-$VERSION_NUMBER/RPMS/x86_64/ksnip-*.rpm ksnip-$VERSION-x86_64.rpm
+    mv ksnip-$VERSION_NUMBER/RPMS/x86_64/ksnip-*.rpm ksnip-$VERSION.rpm
 elif [[ "${BUILD_TYPE}" == "exe" ]]; then
     mkdir build && cd build
     cmake .. -G"NMake Makefiles" -DCMAKE_CXX_COMPILER=cl -DVERSION_SUFIX=$VERSION_SUFFIX -DBUILD_NUMBER=$BUILD_NUMBER -DCMAKE_BUILD_TYPE=Release
@@ -18,7 +20,7 @@ elif [[ "${BUILD_TYPE}" == "exe" ]]; then
     mkdir packageDir
     mv build/src/ksnip*.exe packageDir/ksnip.exe
     windeployqt.exe packageDir/ksnip.exe
-    7z a ksnip-$VERSION-windows-x86_64.zip ./packageDir/*
+    7z a ksnip-$VERSION-windows.zip ./packageDir/*
 elif [[ "${BUILD_TYPE}" == "app" ]]; then
     mkdir build && cd build
     cmake .. -DVERSION_SUFIX=$VERSION_SUFFIX -DBUILD_NUMBER=$BUILD_NUMBER -DCMAKE_BUILD_TYPE=Release
@@ -29,5 +31,5 @@ elif [[ "${BUILD_TYPE}" == "app" ]]; then
     mkdir packageDir
     mv build/src/ksnip*.app packageDir/ksnip.app
     macdeployqt packageDir/ksnip.app
-    sudo hdiutil create ksnip-$VERSION-x86_64.dmg -volname "Ksnip" -fs HFS+ -srcfolder packageDir/
+    sudo hdiutil create ksnip-$VERSION.dmg -volname "Ksnip" -fs HFS+ -srcfolder packageDir/
 fi

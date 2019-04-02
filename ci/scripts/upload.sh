2431b000f0ae3ce8268ac8a4628691d0c04daa7f
Damir Porobic <damir_porobic@live.com>
2019-04-02 21:56:04 +0200

    Fix link to travis ci log

--- ci/scripts/upload.sh
+++ ci/scripts/upload.sh
@@ -2,6 +2,9 @@
 
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
 
+# Fix link to travis ci, the default links to the private projects section, we use travis-ci.org for public open source
+export UPLOADTOOL_BODY="Travis CI build log: https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}/"
+
 if [[ "${BUILD_TYPE}" == "AppImage" ]]; then
     find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
     bash upload.sh ksnip*.AppImage*

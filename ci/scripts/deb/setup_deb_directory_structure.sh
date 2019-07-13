e0d8bc624b75c0495e358ec929ed1c30fe81254f
Damir Porobic <damir_porobic@live.com>
2019-07-13 17:21:57 +0200

    Copy cmake directory to rpm build directory

--- ci/scripts/deb/setup_deb_directory_structure.sh
+++ ci/scripts/deb/setup_deb_directory_structure.sh
@@ -2,7 +2,7 @@
 
 echo "--> Create directory and copy everything we need to deliver"
 mkdir ksnip-${VERSION_NUMBER}
-cp -R CMakeLists.txt desktop/ icons/ LICENSE README.md src/ translations/ ksnip-${VERSION_NUMBER}/
+cp -R CMakeLists.txt cmake/ desktop/ icons/ LICENSE README.md src/ translations/ ksnip-${VERSION_NUMBER}/
 echo "--> Package source content"
 tar -cvzf ksnip_${VERSION_NUMBER}.orig.tar.gz ksnip-${VERSION_NUMBER}/
 echo "--> Copy source package to debian directory"

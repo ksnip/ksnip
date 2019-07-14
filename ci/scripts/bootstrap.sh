b03eee7a8eb840d42d58d23e4ec6333b75aa197a
Damir Porobic <damir_porobic@live.com>
2019-07-14 16:37:15 +0200

    Add missing openssl libs to windows package. Fix #173

--- ci/scripts/bootstrap.sh
+++ ci/scripts/bootstrap.sh
@@ -16,7 +16,7 @@ elif [[ "${BUILD_TYPE}" == "rpm" ]]; then
 elif [[ "${BUILD_TYPE}" == "exe" ]]; then
     source ci/scripts/exe/bootstrap_envvars.sh
 
-    echo "--> Download Qt"
+    echo "--> Download Dependencies"
 
     QT_BASE_URL="https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_563/qt.563.win32_msvc2015/5.6.3-0-201709190903"
     wget --quiet -O qtbase.7z "${QT_BASE_URL}qtbase-Windows-Windows_10-MSVC2015-Windows-Windows_10-X86.7z"
@@ -24,12 +24,15 @@ elif [[ "${BUILD_TYPE}" == "exe" ]]; then
     wget --quiet -O qttools.7z "${QT_BASE_URL}qttools-Windows-Windows_10-MSVC2015-Windows-Windows_10-X86.7z"
     wget --quiet -O qttranslations.7z "${QT_BASE_URL}qttranslations-Windows-Windows_10-MSVC2015-Windows-Windows_10-X86.7z"
     wget --quiet -O qtsvg.7z "${QT_BASE_URL}qtsvg-Windows-Windows_10-MSVC2015-Windows-Windows_10-X86.7z"
+	wget --quiet -O openssl.zip "https://indy.fulgan.com/SSL/openssl-1.0.2q-i386-win32.zip"
 
     7z x qtbase.7z -o/c/qt
     7z x qtwinextras.7z -o/c/qt
     7z x qttools.7z -o/c/qt
     7z x qttranslations.7z -o/c/qt
     7z x qtsvg.7z -o/c/qt
+	
+    7z x openssl.zip -o/c/openssl
 
     # windeployqt fails to find the binaries so we link the binaries where its looking for it
     WINDEPLOYQT_QT_DIR="/c/Users/qt/work"

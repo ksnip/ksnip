47654094204b36bf31189699c1686634066c8bb5
Damir Porobic <damir_porobic@live.com>
2020-04-05 21:08:39 +0200

    Revert "Remove setting desktop filename and change desktop filename to freedesktop format #269"
    This reverts commit c07eb3ccbdcd1bacaedee09ffde5019581db22c8.

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
-#include <QApplication>
+#include <QGuiApplication>
 
 #include "BuildConfig.h"
 #include "src/gui/MainWindow.h"
@@ -36,10 +36,11 @@ int main(int argc, char** argv)
     QApplication app(argc, argv);
     app.setAttribute(Qt::AA_UseHighDpiPixmaps);
 
-	app.setOrganizationName(QStringLiteral("ksnip"));
-	app.setOrganizationDomain(QStringLiteral("ksnip.org"));
-	app.setApplicationName(QStringLiteral("ksnip"));
-	app.setApplicationVersion(QStringLiteral(KSNIP_VERSION));
+	QGuiApplication::setOrganizationName(QStringLiteral("ksnip"));
+	QGuiApplication::setOrganizationDomain(QStringLiteral("ksnip.org"));
+	QGuiApplication::setApplicationName(QStringLiteral("ksnip"));
+	QGuiApplication::setApplicationVersion(QStringLiteral(KSNIP_VERSION));
+	QGuiApplication::setDesktopFileName(QStringLiteral("ksnip.desktop"));
 
     app.setStyle(KsnipConfigProvider::instance()->applicationStyle());
 

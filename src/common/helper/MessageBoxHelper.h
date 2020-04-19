36abf91b08b34c88202ef759768a2eca3ee4c3a7
Damir Porobic <damir_porobic@live.com>
2020-04-19 22:08:15 +0200

    Fix license version in h and cpp files

--- src/common/helper/MessageBoxHelper.h
+++ src/common/helper/MessageBoxHelper.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2018 Damir Porobic <damir.porobic@gmx.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
@@ -11,7 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.

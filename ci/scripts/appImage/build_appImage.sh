16b984e2a862d330619fae2bc0645df298d3ee6c
Damir Porobic <damir_porobic@live.com>
2020-05-10 10:35:06 +0200

    Package ssl library with appImage #320

--- ci/scripts/appImage/build_appImage.sh
+++ ci/scripts/appImage/build_appImage.sh
@@ -3,7 +3,11 @@
 echo "--> Build "
 cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr -DVERSION_SUFIX=${VERSION_SUFFIX} -DBUILD_NUMBER=${BUILD_NUMBER}
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+
+echo "--> Copy SSL libs to appDir"
+cp /lib/x86_64-linux-gnu/libssl.so.1.0.0 appdir/usr/lib
+
 echo "--> Package appImage"
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines,imageformats

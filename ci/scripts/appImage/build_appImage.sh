3f2898b61781d828ce85b0d285b6f30cf5b5e4cf
Damir Porobic <damir_porobic@live.com>
2020-06-19 14:15:40 +0200

    Package kimageannotator translations with appImage

--- ci/scripts/appImage/build_appImage.sh
+++ ci/scripts/appImage/build_appImage.sh
@@ -8,6 +8,10 @@ echo "--> Copy SSL libs to appDir"
 mkdir -p appdir/usr/lib/
 cp /lib/x86_64-linux-gnu/libssl.so.1.0.0 appdir/usr/lib/
 
+echo "--> Copy kImageAnnotator translations to appDir"
+mkdir -p appdir/usr/share/kImageAnnotator/
+cp -r /usr/local/kImageAnnotator/translations appdir/usr/share/kImageAnnotator/
+
 echo "--> Package appImage"
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs

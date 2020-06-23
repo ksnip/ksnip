8c2d400f478586087cb0e376d709d1c3ef494876
Damir Porobic <damir_porobic@live.com>
2020-06-23 18:09:57 +0200

    Fix path to kimageannotator translations in appimage #118

--- ci/scripts/appImage/build_appImage.sh
+++ ci/scripts/appImage/build_appImage.sh
@@ -10,7 +10,7 @@ cp /lib/x86_64-linux-gnu/libssl.so.1.0.0 appdir/usr/lib/
 
 echo "--> Copy kImageAnnotator translations to appDir"
 mkdir -p appdir/usr/share/kImageAnnotator/
-cp -r /usr/local/kImageAnnotator/translations appdir/usr/share/kImageAnnotator/
+cp -r /usr/local/share/kImageAnnotator/translations appdir/usr/share/kImageAnnotator/
 
 echo "--> Package appImage"
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH

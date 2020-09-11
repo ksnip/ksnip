397ab887c63c64e8aaff1b66a3f566e97bb7aff5
Damir Porobic <damir_porobic@live.com>
2020-09-11 08:42:00 +0200

    Make continuous releases pre-releases again

--- ci/scripts/upload.sh
+++ ci/scripts/upload.sh
@@ -5,6 +5,12 @@ wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
 # Fix link to travis ci, the default links to the private projects section, we use travis-ci.org for public open source
 export UPLOADTOOL_BODY="Travis CI build log: https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}/"
 
+# Mark continuous builds as pre-release in github
+if [[ "${VERSION_SUFFIX}" == "continuous" ]]; then
+    export UPLOADTOOL_ISPRERELEASE="true"
+fi
+
+
 if [[ "${BINARY_TYPE}" == "AppImage" ]]; then
     find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
     bash upload.sh ksnip*.AppImage*

6dadbdefd99757d8c4a6abee5428623e9643032a
Damir Porobic <damir_porobic@live.com>
2020-12-20 14:05:55 +0100

    Try reseting xcode paths

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -46,5 +46,6 @@ elif [[ "${BINARY_TYPE}" == "app" ]]; then
     sudo hdiutil create ksnip-${VERSION}.dmg -volname "Ksnip" -fs HFS+ -srcfolder packageDir/
 
     echo "--> Start Notatization process"
+    sudo xcode-select -r
     xcrun altool -t osx -f ksnip-${VERSION}.dmg –primary-bundle-id org.ksnip.ksnip –notarize-app –username ${APPLE_DEV_USER} -password ${APPLE_DEV_PASS}
 fi

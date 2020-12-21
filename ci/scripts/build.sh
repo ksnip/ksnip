76661b150d4f9acede3e4d8faa21e813b57e4860
Damir Porobic <damir_porobic@live.com>
2020-12-21 21:16:30 +0100

    Add loop that check for notarization status

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -40,10 +40,28 @@ elif [[ "${BINARY_TYPE}" == "app" ]]; then
 
     echo "--> Start Notarization process"
     response=$(xcrun altool -t osx -f ksnip-${VERSION}.dmg --primary-bundle-id org.ksnip.ksnip --notarize-app -u ${APPLE_DEV_USER} -p ${APPLE_DEV_PASS})
-    requestUUID=$(echo ${response} | tr ' ' '\n' | tail -1)
-    echo "--> Request Notarization"
-    xcrun altool --notarization-info ${requestUUID} -u ${APPLE_DEV_USER} -p ${APPLE_DEV_PASS}
+    requestUUID=$(echo "${response}" | tr ' ' '\n' | tail -1)
 
-    echo "--> Stable the notarization result to the dmg file"
-    xcrun stapler staple -v ksnip-${VERSION}.dmg
+    while true; do
+      echo "--> Checking notarization status"
+
+      statusCheckResponse=$(xcrun altool --notarization-info ${requestUUID} -u ${APPLE_DEV_USER} -p ${APPLE_DEV_PASS})
+
+      isSuccess=$(echo "${statusCheckResponse}" | grep "success")
+      isFailure=$(echo "${statusCheckResponse}" | grep "invalid")
+
+      if [[ "${isSuccess}" != "" ]]; then
+          echo "Notarization done!"
+          xcrun stapler staple -v ksnip-${VERSION}.dmg
+          echo "Stapler done!"
+          break
+      fi
+      if [[ "${isFailure}" != "" ]]; then
+          echo "Notarization failed"
+          return 1
+      fi
+      echo "Notarization not finished yet, sleep 2m then check again..."
+      sleep 120
+    done
+
 fi

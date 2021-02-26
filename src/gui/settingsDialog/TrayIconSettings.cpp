1cabcba79dfd2f81c0dd5f8abd4a1601fcc6a823
Stefan Comanescu <fnkabit@gmail.com>
2021-02-26 09:03:22 +0200

    Add tooltip for 'Use platform specific notification service' checkbox

--- src/gui/settingsDialog/TrayIconSettings.cpp
+++ src/gui/settingsDialog/TrayIconSettings.cpp
@@ -78,6 +78,8 @@ void TrayIconSettings::initGui()
 	mCloseToTrayCheckBox->setText(tr("Close to Tray"));
 	mTrayIconNotificationsCheckBox->setText(tr("Display Tray icon notifications"));
 	mUsePlatformSpecificNotificationServiceCheckBox->setText(tr("Use platform specific notification service"));
+	mUsePlatformSpecificNotificationServiceCheckBox->setToolTip(tr("When enabled will use the platform specific notification service.\n"
+																   "Change requires restart."));
 
 	connect(mUseTrayIconCheckBox, &QCheckBox::stateChanged, this, &TrayIconSettings::useTrayIconChanged);
 

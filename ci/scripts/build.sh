cad8e53f53cf8c31ccd84fe4ea616a811bdf7b51
Damir Porobic <damir_porobic@live.com>
2021-03-10 21:55:24 +0100

    Remove debug output and use qt5 deploy tool for mac

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -31,17 +31,11 @@ elif [[ "${BINARY_TYPE}" == "app" ]]; then
     make
     cd ..
 
-    echo "--> DEBUG"
-    macdeployqt -v
-    echo "--> DEBUG"
-    ls /usr/local/opt/qt@5/bin
-    echo "--> DEBUG"
-
     echo "--> Package MacOS"
     mv build/src/ksnip*.app ksnip.app
     cp build/translations/ksnip_*.qm ./ksnip.app/Contents/Resources/
     cp build/translations/kImageAnnotator_*.qm ./ksnip.app/Contents/Resources/
-    macdeployqt ksnip.app -dmg -codesign="${APPLE_DEV_IDENTITY}"
+    /usr/local/opt/qt@5/bin/macdeployqt ksnip.app -dmg -sign-for-notarization="${APPLE_DEV_IDENTITY}"
     mv ksnip.dmg ksnip-${VERSION}.dmg
 
     echo "--> Notarize MacOS package"

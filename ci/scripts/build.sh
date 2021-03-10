f04ea984e39f9ed51a636e510eebb5c37900b533
Damir Porobic <damir_porobic@live.com>
2021-03-10 19:36:19 +0100

    Try split macos sign parameter in multiple param

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -35,7 +35,7 @@ elif [[ "${BINARY_TYPE}" == "app" ]]; then
     mv build/src/ksnip*.app ksnip.app
     cp build/translations/ksnip_*.qm ./ksnip.app/Contents/Resources/
     cp build/translations/kImageAnnotator_*.qm ./ksnip.app/Contents/Resources/
-    macdeployqt ksnip.app -dmg -sign-for-notarization="${APPLE_DEV_IDENTITY}"
+    macdeployqt ksnip.app -dmg -hardened-runtime -timestamp -codesign=="${APPLE_DEV_IDENTITY}"
     mv ksnip.dmg ksnip-${VERSION}.dmg
 
     echo "--> Notarize MacOS package"

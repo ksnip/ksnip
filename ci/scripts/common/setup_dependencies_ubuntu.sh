ac7e12d807acb352a69817c3a40aa16ce3d932c7
Damir Porobic <damir_porobic@live.com>
2021-05-19 21:52:51 +0200

    Install ECM from repo on ubuntu

--- ci/scripts/common/setup_dependencies_ubuntu.sh
+++ ci/scripts/common/setup_dependencies_ubuntu.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "--> Install Extra CMake Modules"
-git clone --depth 1 v5.81.0 git://github.com/KDE/extra-cmake-modules
-cd extra-cmake-modules
-mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
-make && sudo make install
-cd ../..
-
 echo "--> Install kColorPicker"
 cd kColorPicker
 mkdir build && cd build
--- ci/scripts/common/setup_ubuntu_qt.sh
+++ ci/scripts/common/setup_ubuntu_qt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get -y install mesa-common-dev libglu1-mesa-dev qt59base qt59x11extras qt59tools qt59svg
+sudo apt-get -y install mesa-common-dev libglu1-mesa-dev qt59base qt59x11extras qt59tools qt59svg extra-cmake-modules
 source /opt/qt*/bin/qt*-env.sh

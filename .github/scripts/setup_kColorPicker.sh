65a18b3efeed850b2a2bc20c5a5eab93ce905f21
Damir Porobic <d.porobic@it-frankfurt.com>
2021-07-26 18:23:09 +0200

    Use name as set for env vars

--- .github/scripts/setup_kColorPicker.sh
+++ .github/scripts/setup_kColorPicker.sh
@@ -5,5 +5,5 @@ git clone --depth 1 git://github.com/ksnip/kColorPicker
 
 cd kColorPicker || exit
 mkdir build && cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE="${ENV_BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${ENV_INSTALL_PREFIX}"
+cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
 make && sudo make install
--- .github/scripts/setup_kImageAnnotator.sh
+++ .github/scripts/setup_kImageAnnotator.sh
@@ -5,5 +5,5 @@ git clone --depth 1 git://github.com/ksnip/kImageAnnotator
 
 cd kImageAnnotator || exit
 mkdir build && cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE="${ENV_BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${ENV_INSTALL_PREFIX}" -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES="${ENV_INSTALL_PREFIX}/include/kColorPicker"
+cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES="${INSTALL_PREFIX}/include/kColorPicker"
 make && sudo make install

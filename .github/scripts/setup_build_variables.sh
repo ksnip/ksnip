687e10a7ba895c6c68bcf96a9eeae9ce94a7dec4
Damir Porobic <d.porobic@it-frankfurt.com>
2021-07-26 11:05:35 +0200

    Extract build variable setup into script

---
+++ .github/scripts/setup_build_variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION_REGEX="([0-9]{1,}\.)+[0-9]{1,}"
+BUILD_TIME=$(date +"%a, %d %b %Y %T %z")
+BUILD_DATE=$(date  +"%a %b %d %Y")
+BUILD_NUMBER=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
+VERSION_NUMBER=$(grep "project.*" CMakeLists.txt | egrep -o "${VERSION_REGEX}")
+INSTALL_PREFIX="${{ github.workspace }}/tmp"
+
+# Store variables in files that is shared among runners.
+echo "BUILD_TYPE=Release" >> $GITHUB_ENV
+echo "BUILD_TIME=$BUILD_TIME" >> $GITHUB_ENV
+echo "BUILD_DATE=$BUILD_DATE" >> $GITHUB_ENV
+echo "BUILD_NUMBER=$BUILD_NUMBER" >> $GITHUB_ENV
+echo "VERSION_NUMBER=$VERSION_NUMBER" >> $GITHUB_ENV
+echo "INSTALL_PREFIX=$INSTALL_PREFIX" >> $GITHUB_ENV

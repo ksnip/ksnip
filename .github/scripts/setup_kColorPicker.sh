2c2f1c02a69cd2991f8d9cdf01a9e78ff3a9b214
Damir Porobic <d.porobic@it-frankfurt.com>
2021-07-27 11:24:54 +0200

    Pass cmake generator to build script

--- .github/scripts/setup_kColorPicker.sh
+++ .github/scripts/setup_kColorPicker.sh
@@ -5,7 +5,7 @@ git clone --depth 1 git://github.com/ksnip/kColorPicker
 
 cd kColorPicker || exit
 mkdir build && cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
+cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -G"${1}"
 echo "--------------"
 ls -al
 echo "--------------"

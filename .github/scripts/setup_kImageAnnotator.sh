6f54d6b3300e168810b3a571d2dd1b1b4e4d4472
Damir Porobic <d.porobic@it-frankfurt.com>
2021-08-03 10:02:58 +0200

    Use verbose flag

--- .github/scripts/setup_kImageAnnotator.sh
+++ .github/scripts/setup_kImageAnnotator.sh
@@ -5,5 +5,5 @@ git clone --depth 1 git://github.com/ksnip/kImageAnnotator
 
 cd kImageAnnotator || exit
 mkdir build && cd build || exit
-cmake .. -G"${1}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES="${INSTALL_PREFIX}/include/kColorPicker"
+cmake .. -G"${1}" --verbose -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES="${INSTALL_PREFIX}/include/kColorPicker"
 make && make install

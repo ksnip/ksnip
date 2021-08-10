8304914e99b2d985dd0677c39f074c02fb05f3cf
Damir Porobic <d.porobic@it-frankfurt.com>
2021-08-10 08:47:35 +0200

    Define env vars for step

--- .github/scripts/setup_build_variables.sh
+++ .github/scripts/setup_build_variables.sh
@@ -6,7 +6,6 @@ BUILD_DATE=$(date  +"%a %b %d %Y")
 BUILD_NUMBER=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
 VERSION_NUMBER=$(grep "project.*" CMakeLists.txt | egrep -o "${VERSION_REGEX}")
 INSTALL_PREFIX="$GITHUB_WORKSPACE/tmp"
-LIB_DIR="$INSTALL_PREFIX/lib"
 
 # Store variables in files that is shared among runners.
 echo "BUILD_TYPE=Release" >> $GITHUB_ENV
@@ -15,4 +14,3 @@ echo "BUILD_DATE=$BUILD_DATE" >> $GITHUB_ENV
 echo "BUILD_NUMBER=$BUILD_NUMBER" >> $GITHUB_ENV
 echo "VERSION_NUMBER=$VERSION_NUMBER" >> $GITHUB_ENV
 echo "INSTALL_PREFIX=$INSTALL_PREFIX" >> $GITHUB_ENV
-echo "CMAKE_LIBRARY_PATH=$LIB_DIR" >> $GITHUB_ENV

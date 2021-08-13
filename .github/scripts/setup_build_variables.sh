dd8bb1e3510449c28ce1203c89e4a75757a1aee7
Damir Porobic <d.porobic@it-frankfurt.com>
2021-08-13 18:33:37 +0200

    Add verbose flag

--- .github/scripts/setup_build_variables.sh
+++ .github/scripts/setup_build_variables.sh
@@ -8,7 +8,7 @@ VERSION_NUMBER=$(grep "project.*" CMakeLists.txt | egrep -o "${VERSION_REGEX}")
 INSTALL_PREFIX="$GITHUB_WORKSPACE/tmp"
 
 # Store variables in files that is shared among runners.
-echo "BUILD_TYPE=Release" >> $GITHUB_ENV
+echo "BUILD_TYPE=Debug" >> $GITHUB_ENV
 echo "BUILD_TIME=$BUILD_TIME" >> $GITHUB_ENV
 echo "BUILD_DATE=$BUILD_DATE" >> $GITHUB_ENV
 echo "BUILD_NUMBER=$BUILD_NUMBER" >> $GITHUB_ENV

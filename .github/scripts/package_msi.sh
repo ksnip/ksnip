65f6b4bb6a41c694a5f3965cec276e40fa2b72b1
Damir Porobic <damir_porobic@live.com>
2021-08-21 15:42:15 +0200

    Run install to check if variables are read

--- .github/scripts/package_msi.sh
+++ .github/scripts/package_msi.sh
@@ -5,5 +5,10 @@ export QTDIR="${QT5_DIR}"
 export OPENSSL_DIR="${GITHUB_WORKSPACE}/open_ssl"
 
 cd build
+
+nmake install
+
+echo "----------------------"
+
 cpack --verbose
 mv ksnip*.msi ${GITHUB_WORKSPACE}/ksnip-${VERSION}.msi

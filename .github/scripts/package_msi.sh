10a62b3296a976274a3ef5cf019e24f760a92a28
Damir Porobic <damir_porobic@live.com>
2021-08-21 14:28:23 +0200

    Export environment variables required for cpack

--- .github/scripts/package_msi.sh
+++ .github/scripts/package_msi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export KIMAGEANNOTATOR="${INSTALL_PREFIX}"
+export QTDIR="${QT5_DIR}"
+export OPENSSL_DIR="${GITHUB_WORKSPACE}/open_ssl"
+
 cd build
 cpack --verbose
-mv ksnip*.msi $GITHUB_WORKSPACE/ksnip-${VERSION}.msi
+mv ksnip*.msi ${GITHUB_WORKSPACE}/ksnip-${VERSION}.msi

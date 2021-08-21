a43384e2aa03dfae9bac9c441b2a450b57a131f7
Damir Porobic <damir_porobic@live.com>
2021-08-21 19:53:37 +0200

    Remove debug outputs

--- .github/scripts/package_msi.sh
+++ .github/scripts/package_msi.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-export KIMAGEANNOTATOR="${INSTALL_PREFIX}"
-export QTDIR="${QT5_DIR}"
-export OPENSSL_DIR="${GITHUB_WORKSPACE}/open_ssl"
-
 cd build
-
-nmake install
-
-echo "----------------------"
-cat ./cmake_install.cmake
-echo "----------------------"
-cat ./src/cmake_install.cmake
-echo "----------------------"
 
 cpack --verbose
 mv ksnip*.msi ${GITHUB_WORKSPACE}/ksnip-${VERSION}.msi
--- .github/scripts/setup_build_variables.sh
+++ .github/scripts/setup_build_variables.sh
@@ -6,13 +6,15 @@ BUILD_DATE=$(date  +"%a %b %d %Y")
 BUILD_NUMBER=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
 VERSION_NUMBER=$(grep "project.*" CMakeLists.txt | egrep -o "${VERSION_REGEX}")
 VERSION_SUFFIX="continuous"
-INSTALL_PREFIX="$GITHUB_WORKSPACE/tmp"
+WORKSPACE="$GITHUB_WORKSPACE"
+INSTALL_PREFIX="$WORKSPACE/tmp"
 
 # Store variables in files that is shared among runners.
 echo "BUILD_TYPE=Release" >> $GITHUB_ENV
 echo "BUILD_TIME=$BUILD_TIME" >> $GITHUB_ENV
 echo "BUILD_DATE=$BUILD_DATE" >> $GITHUB_ENV
 echo "BUILD_NUMBER=$BUILD_NUMBER" >> $GITHUB_ENV
+echo "WORKSPACE=$WORKSPACE" >> $GITHUB_ENV
 echo "INSTALL_PREFIX=$INSTALL_PREFIX" >> $GITHUB_ENV
 echo "VERSION_NUMBER=$VERSION_NUMBER" >> $GITHUB_ENV
 echo "VERSION_SUFFIX=$VERSION_SUFFIX" >> $GITHUB_ENV

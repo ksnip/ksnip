4621d0e32188d6a8fe60e81fe11373410b0edc20
Damir Porobic <damir_porobic@live.com>
2021-08-28 19:15:31 +0200

    Pass cmake as script argument

--- .github/scripts/setup_kColorPicker.sh
+++ .github/scripts/setup_kColorPicker.sh
@@ -5,5 +5,5 @@ git clone --depth 1 git://github.com/ksnip/kColorPicker
 
 cd kColorPicker || exit
 mkdir build && cd build || exit
-cmake .. -G"${1}" -D CMAKE_CXX_FLAGS="-D_WIN32_WINNT=0x0A00" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
+${2} .. -G"${1}" -D CMAKE_CXX_FLAGS="-D_WIN32_WINNT=0x0A00" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
 make && make install

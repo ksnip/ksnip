309fbc60d24dc8fef437b942916e15bfba741636
Damir Porobic <damir_porobic@live.com>
2021-08-28 19:23:45 +0200

    Pass make instead of cmake to scripts and use scripts for windows

--- .github/scripts/setup_kImageAnnotator.sh
+++ .github/scripts/setup_kImageAnnotator.sh
@@ -5,5 +5,5 @@ git clone --depth 1 git://github.com/ksnip/kImageAnnotator
 
 cd kImageAnnotator || exit
 mkdir build && cd build || exit
-cmake .. -G"${1}" -D CMAKE_CXX_FLAGS="-D_WIN32_WINNT=0x0A00" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES="${INSTALL_PREFIX}/include"
-make && make install
+cmake .. -G"${1}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DBUILD_EXAMPLE=OFF -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES="${INSTALL_PREFIX}/include"
+${2} && ${2} install

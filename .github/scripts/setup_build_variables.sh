5693125dabbf48feed7f64690046c28888f6c24e
Damir Porobic <damir_porobic@live.com>
2021-08-28 14:07:10 +0200

    Move setting windows buold vars into own step

--- .github/scripts/setup_build_variables.sh
+++ .github/scripts/setup_build_variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export VERSION_REGEX="([0-9]{1,}\.)+[0-9]{1,}"
-export BUILD_TIME=$(date +"%a, %d %b %Y %T %z")
-export BUILD_DATE=$(date  +"%a %b %d %Y")
-export BUILD_NUMBER=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
-export VERSION_NUMBER=$(grep "project.*" CMakeLists.txt | egrep -o "${VERSION_REGEX}")
-export VERSION_SUFFIX="continuous"
-export WORKSPACE="$GITHUB_WORKSPACE"
-export INSTALL_PREFIX="$WORKSPACE/tmp"
+VERSION_REGEX="([0-9]{1,}\.)+[0-9]{1,}"
+BUILD_TIME=$(date +"%a, %d %b %Y %T %z")
+BUILD_DATE=$(date  +"%a %b %d %Y")
+BUILD_NUMBER=$(git rev-list --count HEAD)-$(git rev-parse --short HEAD)
+VERSION_NUMBER=$(grep "project.*" CMakeLists.txt | egrep -o "${VERSION_REGEX}")
+VERSION_SUFFIX="continuous"
+WORKSPACE="$GITHUB_WORKSPACE"
+INSTALL_PREFIX="$WORKSPACE/tmp"
 
 # Store variables in files that is shared among runners.
 echo "BUILD_TYPE=Release" >> $GITHUB_ENV

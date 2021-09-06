0893c89349f1c8fa9f5989fb4fe51922a3d84b83
Damir Porobic <d.porobic@it-frankfurt.com>
2021-09-06 11:08:13 +0200

    Set deb cmake prefix path as semicolon seperated list

--- .github/scripts/linux/rpm/build_rpm.sh
+++ .github/scripts/linux/rpm/build_rpm.sh
@@ -4,5 +4,5 @@ cd ksnip-${VERSION_NUMBER}
 
 rpmbuild -ba SPECS/ksnip-*.spec --define '_topdir %(pwd)'
 
-mv ksnip-${VERSION_NUMBER}/RPMS/x86_64/ksnip-*.rpm ksnip-${VERSION}.rpm
+mv ${WORKSPACE}/ksnip-${VERSION_NUMBER}/RPMS/x86_64/ksnip-*.rpm ksnip-${VERSION}.rpm
 

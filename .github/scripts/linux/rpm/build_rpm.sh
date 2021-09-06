7d7da63c0feb5e61334d9fb0b93015d18e4f86a8
Damir Porobic <d.porobic@it-frankfurt.com>
2021-09-06 12:19:31 +0200

    Change cmake prefix path position

--- .github/scripts/linux/rpm/build_rpm.sh
+++ .github/scripts/linux/rpm/build_rpm.sh
@@ -4,13 +4,4 @@ cd ksnip-${VERSION_NUMBER}
 
 rpmbuild -ba SPECS/ksnip-*.spec --define '_topdir %(pwd)'
 
-echo "----------------"
-ls ${WORKSPACE}/ksnip-${VERSION_NUMBER}/RPMS/
-echo "----------------"
-pwd
-echo "----------------"
 mv ${WORKSPACE}/ksnip-${VERSION_NUMBER}/RPMS/x86_64/ksnip-*.rpm ${WORKSPACE}/ksnip-${VERSION}.rpm
-echo "----------------"
-ls ${WORKSPACE}/
-echo "----------------"
-

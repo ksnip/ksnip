ebb4e3923cf9ea5d0bcc42397ade3dffd8727d1d
Damir Porobic <d.porobic@it-frankfurt.com>
2021-09-06 11:41:12 +0200

    Use different delimiter for setting build rules via sed

--- .github/scripts/linux/rpm/build_rpm.sh
+++ .github/scripts/linux/rpm/build_rpm.sh
@@ -4,5 +4,13 @@ cd ksnip-${VERSION_NUMBER}
 
 rpmbuild -ba SPECS/ksnip-*.spec --define '_topdir %(pwd)'
 
-mv ${WORKSPACE}/ksnip-${VERSION_NUMBER}/RPMS/x86_64/ksnip-*.rpm ksnip-${VERSION}.rpm
+echo "----------------"
+ls ${WORKSPACE}/ksnip-${VERSION_NUMBER}/RPMS/
+echo "----------------"
+pwd
+echo "----------------"
+mv ${WORKSPACE}/ksnip-${VERSION_NUMBER}/RPMS/x86_64/ksnip-*.rpm ${WORKSPACE}/ksnip-${VERSION}.rpm
+echo "----------------"
+ls ${WORKSPACE}/
+echo "----------------"
 

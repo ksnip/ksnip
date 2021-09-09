29c5f9c80811a2e665c9f98e71f5055bd3740f5b
Damir Porobic <d.porobic@it-frankfurt.com>
2021-09-09 19:13:46 +0200

    Move deb package to workspace directory

--- .github/scripts/linux/deb/build_deb.sh
+++ .github/scripts/linux/deb/build_deb.sh
@@ -4,8 +4,10 @@ cd ksnip-${VERSION_NUMBER}
 
 debuild -us -uc --lintian-opts --profile debian
 
-mv ksnip_*.deb ksnip-${VERSION}.deb
+mv ../ksnip_*.deb ${WORKSPACE}/ksnip-${VERSION}.deb
 
+echo "-----------"
+pwd
 echo "-----------"
 ls .. -al
 echo "-----------"

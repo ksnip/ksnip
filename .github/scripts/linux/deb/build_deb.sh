c500ae307a1f90f2422dc69f75c05aaae45becee
Damir Porobic <d.porobic@it-frankfurt.com>
2021-09-10 08:44:50 +0200

    Specify env var to be preserved for deb build

--- .github/scripts/linux/deb/build_deb.sh
+++ .github/scripts/linux/deb/build_deb.sh
@@ -2,7 +2,7 @@
 
 cd ksnip-${VERSION_NUMBER}
 
-debuild -us -uc --lintian-opts --profile debian --preserve-env
+debuild -us -uc --lintian-opts --profile debian --preserve-envvar VERSION_SUFFIX
 
 mv ${WORKSPACE}/ksnip_*.deb ${WORKSPACE}/ksnip-${VERSION}.deb
 

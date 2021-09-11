81e62fa8652d0dd38c7558b626d09f3845505d6b
Damir Porobic <damir_porobic@live.com>
2021-09-11 23:23:06 +0200

    Limit notarization retry to 10

--- .github/scripts/macos/notarize_osx_dmg_package.sh
+++ .github/scripts/macos/notarize_osx_dmg_package.sh
@@ -4,7 +4,9 @@ echo "Starting Notarization process."
 response=$(xcrun altool -t osx -f ksnip-${VERSION}.dmg --primary-bundle-id org.ksnip.ksnip --notarize-app -u ${APPLE_DEV_USER} -p ${APPLE_DEV_PASS})
 requestUUID=$(echo "${response}" | tr ' ' '\n' | tail -1)
 
-while true; do
+retryCounter=0
+
+until [ "$retryCounter" -ge 10 ]; do
   echo "Checking notarization status."
   statusCheckResponse=$(xcrun altool --notarization-info ${requestUUID} -u ${APPLE_DEV_USER} -p ${APPLE_DEV_PASS})
 
@@ -17,10 +19,14 @@ while true; do
       echo "Stapler done!"
       break
   fi
+
   if [[ "${isFailure}" != "" ]]; then
       echo "Notarization failed!"
       return 1
   fi
+
+  retryCounter=$((retryCounter + 1))
+
   echo "Notarization not finished yet, sleep 2min then check again..."
   sleep 120
 done

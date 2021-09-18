dcabc5db5774f2ad2234636fd651026ccf3b83d8
Damir Porobic <damir_porobic@live.com>
2021-09-18 10:58:47 +0200

    Set winddeployqt flags #713

--- .github/scripts/windows/package_exe.sh
+++ .github/scripts/windows/package_exe.sh
@@ -3,7 +3,7 @@
 mkdir packageDir
 mv build/src/ksnip*.exe packageDir/ksnip.exe
 
-windeployqt.exe packageDir/ksnip.exe
+windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --no-compiler-runtime --release packageDir/ksnip.exe
 
 cp build/translations/ksnip_*.qm ./packageDir/translations/
 cp kImageAnnotator/build/translations/kImageAnnotator_*.qm ./packageDir/translations/

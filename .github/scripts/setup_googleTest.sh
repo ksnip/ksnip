325bbe6f5507ec703e1266a1559c41452ea285fc
Damir Porobic <damir_porobic@live.com>
2021-10-06 22:43:51 +0200

    Change test runner from make test to ctest

--- .github/scripts/setup_googleTest.sh
+++ .github/scripts/setup_googleTest.sh
@@ -3,5 +3,5 @@
 git clone --depth 1 https://github.com/google/googletest
 cd googletest || exit
 mkdir build && cd build || exit
-cmake .. -G"${CMAKE_GENERATOR}" -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
+cmake .. -G"${CMAKE_GENERATOR}" -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" -DBUILD_SHARED_LIBS=ON
 ${MAKE_BINARY} && ${MAKE_BINARY} install

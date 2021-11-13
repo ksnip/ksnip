f3758f7bbf0cd630501931a2d200d8bb97c8b369
Damir Porobic <damir_porobic@live.com>
2021-11-13 22:37:14 +0100

    Add debug outputs for tracking vcruntime

--- .github/scripts/windows/package_exe.sh
+++ .github/scripts/windows/package_exe.sh
@@ -3,7 +3,11 @@
 mkdir packageDir
 mv build/src/ksnip*.exe packageDir/ksnip.exe
 
-windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --release packageDir/ksnip.exe
+echo "---------------"
+find / -name VCRUNTIME140_1.dll
+echo "---------------"
+
+windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --compiler-runtime --release packageDir/ksnip.exe
 
 cp build/translations/ksnip_*.qm ./packageDir/translations/
 cp kImageAnnotator/build/translations/kImageAnnotator_*.qm ./packageDir/translations/

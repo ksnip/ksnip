902735c8b710d446662aff4230c628a538a66a00
Damir Porobic <damir_porobic@live.com>
2021-11-14 17:30:53 +0100

    Search for run time compile under c

--- .github/scripts/windows/package_exe.sh
+++ .github/scripts/windows/package_exe.sh
@@ -4,7 +4,7 @@ mkdir packageDir
 mv build/src/ksnip*.exe packageDir/ksnip.exe
 
 echo "---------------"
-find / -name VCRUNTIME140_1.dll
+find /c -name VCRUNTIME140_1.dll
 echo "---------------"
 
 windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --compiler-runtime --release packageDir/ksnip.exe

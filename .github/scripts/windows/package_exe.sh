199b97b68861ec22b1add581d5e22ce3430cb82b
Damir Porobic <d.porobic@it-frankfurt.com>
2021-11-14 21:34:14 +0100

    Fetch dependencies from our own repo

--- .github/scripts/windows/package_exe.sh
+++ .github/scripts/windows/package_exe.sh
@@ -7,17 +7,14 @@ windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --no-compiler-runtime --
 
 cp build/translations/ksnip_*.qm ./packageDir/translations/
 cp kImageAnnotator/build/translations/kImageAnnotator_*.qm ./packageDir/translations/
-cp ${OPENSSL_DIR}/{libeay32.dll,ssleay32.dll} ./packageDir/ || true
+cp ${OPENSSL_DIR}/{libeay32.dll,ssleay32.dll} ./packageDir/
 
 echo "---------------"
-curl -L https://de.dll-files.com/download/7b92a6cb5d2cad407c457ab12d2b211d/msvcp140.dll.html?c=L20zYStrQm0zU1FDekpvTTdxRldJZz09 --output msvcp140.zip
-curl -L https://de.dll-files.com/download/cfc08fca16c3647a42e78ef7556e4090/vcruntime140.dll.html?c=SHY4Z3V4WlZQZ2RzYWFnVHVsNFdVdz09 --output vcruntime140.zip
+curl -L https://github.com/ksnip/dependencies/raw/master/windows/compileRuntime.zip --output compileRuntime.zip
 
-7z x msvcp140.zip || true
-7z x vcruntime140.zip || true
+7z x compileRuntime.zip -o"compileRuntime"
 
-cp msvcp140.dll ./packageDir/ || true
-cp vcruntime140.dll ./packageDir/ || true
+cp ${compileRuntime}/{msvcp140.dll,vcruntime140.dll} ./packageDir/
 echo "---------------"
 
 7z a ksnip-${VERSION}-windows.zip ./packageDir/*

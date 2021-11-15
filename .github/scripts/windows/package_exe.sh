31348873b914502adce6e1e97812f7704b479a7e
Damir Porobic <d.porobic@it-frankfurt.com>
2021-11-15 22:55:28 +0100

    Use wildcard to copy windows dependency dlls

--- .github/scripts/windows/package_exe.sh
+++ .github/scripts/windows/package_exe.sh
@@ -7,14 +7,14 @@ windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --no-compiler-runtime --
 
 cp build/translations/ksnip_*.qm ./packageDir/translations/
 cp kImageAnnotator/build/translations/kImageAnnotator_*.qm ./packageDir/translations/
-cp ${OPENSSL_DIR}/{libcrypto-1_1.dll,libssl-1_1.dll} ./packageDir/
+cp ${OPENSSL_DIR}/*.dll ./packageDir/
 
 echo "---------------"
 curl -L https://github.com/ksnip/dependencies/raw/master/windows/compileRuntime.zip --output compileRuntime.zip
 
 7z x compileRuntime.zip -o"compileRuntime"
 
-cp compileRuntime/{msvcp140.dll,vcruntime140.dll,msvcp140_1.dll,vcruntime140_1.dll} ./packageDir/
+cp compileRuntime/*.dll ./packageDir/
 echo "---------------"
 
 7z a ksnip-${VERSION}-windows.zip ./packageDir/*

809c71931f2616b323f0328da4fd490494dfe372
Damir Porobic <damir_porobic@live.com>
2021-11-19 08:38:47 +0100

    Quote openssl dir when copying files

--- .github/scripts/windows/package_exe.sh
+++ .github/scripts/windows/package_exe.sh
@@ -7,14 +7,16 @@ windeployqt.exe --no-opengl-sw --no-system-d3d-compiler --no-compiler-runtime --
 
 cp build/translations/ksnip_*.qm ./packageDir/translations/
 cp kImageAnnotator/build/translations/kImageAnnotator_*.qm ./packageDir/translations/
-cp ${OPENSSL_DIR}/*.dll ./packageDir/
+cp "${OPENSSL_DIR}"/*.dll ./packageDir/
 
 echo "---------------"
 
 echo "**************************"
-ls
+pwd
 echo "**************************"
-ls ${OPENSSL_DIR}/
+ls -l
+echo "**************************"
+ls ${OPENSSL_DIR}/ -l
 echo "**************************"
 
 curl -L https://github.com/ksnip/dependencies/raw/master/windows/compileRuntime.zip --output compileRuntime.zip

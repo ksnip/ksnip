1f9616fea53fb3dcb77a28e89535efdbd8cccbf0
Henning Andersen <henning.andersen@gmail.com>
2023-10-09 08:12:05 +0000

    Translated using Weblate (Danish)
    Currently translated at 77.9% (314 of 403 strings)
    
    Translation: ksnip/Translations
    Translate-URL: https://hosted.weblate.org/projects/ksnip/translations/da/

--- translations/ksnip_da.ts
+++ translations/ksnip_da.ts
@@ -104,7 +104,7 @@
     </message>
     <message>
         <source>Global</source>
-        <translation type="unfinished"></translation>
+        <translation>Global</translation>
     </message>
     <message>
         <source>When enabled will make the shortcut

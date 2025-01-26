28d339d3266f6f0a57c7d2bffa0e64d27a0118bf
Alexey Sokolov <alexey@asokolov.org>
2025-01-26 19:42:53 +0000

    Maybe fix Qt5 build
    CMake already requires the newer Qt5 than this, so the old branch can be
    removed

--- src/backend/imageGrabber/AbstractRectAreaImageGrabber.cpp
+++ src/backend/imageGrabber/AbstractRectAreaImageGrabber.cpp
@@ -52,16 +52,11 @@ void AbstractRectAreaImageGrabber::grabImage(CaptureModes captureMode, bool capt
  */
 QRect AbstractRectAreaImageGrabber::currentScreenRect() const
 {
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
     auto screen = QGuiApplication::screenAt(QCursor::pos());
     if (screen == nullptr) {
         screen = QGuiApplication::primaryScreen();
     }
     return screen->geometry();
-#else
-    auto screen = QApplication::desktop()->screenNumber(QCursor::pos());
-    return QApplication::desktop()->screenGeometry(screen);
-#endif
 }
 
 QRect AbstractRectAreaImageGrabber::lastRectArea() const

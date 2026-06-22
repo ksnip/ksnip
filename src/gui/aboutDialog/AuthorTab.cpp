363281a82f3d5db16e3e9ebe09e4ffcb9418a4a8
Milo Ivir <43657314+milotype@users.noreply.github.com>
2026-06-22 20:54:18 +0200

    Fix syntax error in contributor entry for Milo Ivir

--- src/gui/aboutDialog/AuthorTab.cpp
+++ src/gui/aboutDialog/AuthorTab.cpp
@@ -36,8 +36,7 @@ AuthorTab::AuthorTab()
 				   createContributorEntry(QLatin1String("4goodapp"), tr("French Translation")) +
 				   createContributorEntry(QLatin1String("epsiloneridani"), tr("Polish Translation")) +
 				   createContributorEntry(QLatin1String("YAMADA Shinichirou, maboroshin"), tr("Japanese Translation")) +
-				   createContributorEntry(QLatin1String("Milo Ivir, milotype"), tr("Croatian Translation")), QLatin1String("mail@milotype.de")) 
-
+				   createContributorEntry(QLatin1String("Milo Ivir, milotype"), tr("Croatian Translation")), QLatin1String("mail@milotype.de"))
 	);
 	mContent->setTextFormat(Qt::RichText);
 	mContent->setTextInteractionFlags(Qt::TextBrowserInteraction);
